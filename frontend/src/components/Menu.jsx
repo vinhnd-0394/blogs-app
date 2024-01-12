de5419b705b1c50f2051b0d02289c493598c252f
vinhnd-0394 <ngu.duy.vinh@sun-asterisk.com>
2024-01-12 10:11:02 +0700

    sample

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,62 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import axios from "axios";
+import { URL } from "../url";
+import { Link, useNavigate } from "react-router-dom";
+
+const Menu = () => {
+  const { user } = useContext(UserContext);
+  const { setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.get(URL + "/api/auth/logout", {
+        withCredentials: true,
+      });
+      setUser(null);
+      navigate("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="bg-black w-[200px] z-10 flex flex-col items-start absolute top-12 right-6 md:right-32 rounded-md p-2">
+      {!user && (
+        <h3 className="text-white text-sm hover:text-black hover:bg-white p-2 space-y-4 w-full cursor-pointer">
+          <Link to="/login">Login</Link>
+        </h3>
+      )}
+      {!user && (
+        <h3 className="text-white text-sm hover:text-black hover:bg-white  p-2 space-y-4 cursor-pointer">
+          <Link to="/register">Register</Link>
+        </h3>
+      )}
+      {user && (
+        <h3 className="text-white text-sm hover:text-black hover:bg-white p-2 space-y-4 w-full cursor-pointer">
+          <Link to={"/profile/" + user._id}>Profile</Link>
+        </h3>
+      )}
+      {user && (
+        <h3 className="text-white text-sm hover:text-black hover:bg-white p-2 space-y-4 w-full cursor-pointer">
+          <Link to="/write">Write new post</Link>
+        </h3>
+      )}
+      {user && (
+        <h3 className="text-white text-sm hover:text-black hover:bg-white p-2 space-y-4 w-full cursor-pointer">
+          <Link to={"/myblogs/" + user._id}>My blogs</Link>
+        </h3>
+      )}
+      {user && (
+        <h3
+          onClick={handleLogout}
+          className="text-white text-sm hover:text-black hover:bg-white p-2 space-y-4 w-full cursor-pointer"
+        >
+          Logout
+        </h3>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
